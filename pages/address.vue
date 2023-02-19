907e0512be650a377e67519aba656b35a74d5a54
Lucas Dourado <douradoblucas@gmail.com>
2023-02-18 21:51:55 -0300

    Feature: Informação Usuário

---
+++ pages/address.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <header>
+            <Navbar />
+        </header>
+        <h1>Meus Endereços</h1>
+
+        <ul>
+            <li>{{ address.data?.cidade }} - {{ address.data?.bairro }}, {{ address.data?.rua }}, Nº {{ address.data?.numero }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        middleware: ['verified'],
+        data: () => ({
+            address: []
+        }),
+        async mounted() {
+            try{
+                this.address = await this.$axios.get('api/address')
+            } catch (e){
+                
+            }
+        },
+    }
+</script>
+
+<style>
+</style>
